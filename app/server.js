b45273e7f0916520320ac5cd951d3cf99c3ca918
essem <soomin@gmail.com>
2015-10-27 12:54:44 +0900

    Install koa

--- app/server.js
+++ app/server.js
@@ -1,2 +1,19 @@
-let [hello, world] = ['hello', 'world'];
-console.log(hello, world);
+import koa from 'koa';
+
+let port = 5000;
+
+let app = koa();
+
+app.use(function*(next){
+  let start = new Date;
+  yield next;
+  let ms = new Date - start;
+  console.log('%s %s - %s ms', this.method, this.url, ms);
+});
+
+app.use(function*() {
+  this.body = 'hello world';
+});
+
+app.listen(port);
+console.log(`server is started on ${port}`);
