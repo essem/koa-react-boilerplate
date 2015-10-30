b6a6c55bab8fac31ac63fafc3acb9229cd054b09
essem <soomin@gmail.com>
2015-10-30 12:13:32 +0900

    Install react-bootstrap

--- app/client/index.js
+++ app/client/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from 'react-bootstrap/lib/Button';
 
-ReactDOM.render(<div>hello world</div>, document.body);
+ReactDOM.render(<Button bsStyle="info">hellow world</Button>, document.body);
