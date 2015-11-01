89560937c6b19e253af99c6245a51ba7a345ed63
essem <soomin@gmail.com>
2015-11-01 20:53:40 +0900

    Install react-router

--- app/client/index.js
+++ app/client/index.js
@@ -1,5 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link } from 'react-router'
 import Button from 'react-bootstrap/lib/Button';
 
-ReactDOM.render(<Button bsStyle="info">hellow world</Button>, document.body);
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <div>hello world</div>
+  }
+}
+
+class About extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <div>about</div>
+  }
+}
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App}>
+      <IndexRoute component={Dashboard} />
+      <Route path="about" component={About} />
+    </Route>
+  </Router>
+), document.getElementById('app'));
